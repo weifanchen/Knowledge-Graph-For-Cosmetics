cd4d56e6eb0b97903c697befa305f96949f18d09
HsinYu <hsinyu0129@gmail.com>
2020-02-25 18:19:46 -0800

    items created

--- sephora/sephora/pipelines.py
+++ sephora/sephora/pipelines.py
@@ -5,7 +5,23 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
-
+import json
 class SephoraPipeline(object):
+
+    def open_spider(self, spider):
+        self.productfile = open('sephora_skincare_product.jl', 'w')
+        self.reviewfile = open('sephora_skincare_review.jl', 'w')
+
+    def close_spider(self, spider):
+        self.productfile.close()
+        self.reviewfile.close()
+
     def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + '\n'
+        if dict(item)['item_type'] == 'product':
+            self.productfile.write(line)
+        else:
+            self.reviewfile.write(line)      
         return item
+
+
