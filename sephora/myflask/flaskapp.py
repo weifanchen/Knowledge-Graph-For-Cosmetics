86f0722ffd1dda1857c3b26d22955a03653f0ce4
HsinYu <hsinyu0129@gmail.com>
2020-04-20 13:25:02 -0700

    add example

--- sephora/myflask/flaskapp.py
+++ sephora/myflask/flaskapp.py
@@ -8,9 +8,19 @@ CORS(app)
 
 @app.route('/', methods=['GET'])
 def ping_pong():
-    print(request.args)
+    res = request.args
+    print(res['type'])
     # return jsonify(queryFormula())
     return jsonify('Hello from Flask!')
+
+
+# Basic Search arguments example
+# res = ImmutableMultiDict([('type', 'Basic'), ('product', 'Protini™ Moisturizer')])
+# 127.0.0.1 - - [20/Apr/2020 13:21:19] "GET /?type=Basic&product=Protini%E2%84%A2+Moisturizer HTTP/1.1" 200 -
+
+# Advanced Search arguments example
+# res = ImmutableMultiDict([('type', 'Advanced'), ('categories[]', 'Moisturizers'), ('categories[]', 'Face Oils'), ('acne', '2'), ('irri', '3'), ('fda[]', 'Fragrance'), ('fda[]', 'Preservative')])
+# 127.0.0.1 - - [20/Apr/2020 13:20:28] "GET /?type=Advanced&categories[]=Moisturizers&categories[]=Face+Oils&acne=2&irri=3&fda[]=Fragrance&fda[]=Preservative HTTP/1.1" 200 -
 
 # @app.route('/test', methods=['GET', 'POST'])
 # def test():
